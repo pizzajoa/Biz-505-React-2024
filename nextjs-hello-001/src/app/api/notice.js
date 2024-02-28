7a499c0f6bef4c3ea5f822b4a6add750b0707b2f
pizzajoa <jjh0954@naver.com>
2024-02-28 15:52:11 +0900

    1

--- nextjs-hello-001/src/app/api/notice.js
+++ nextjs-hello-001/src/app/api/notice.js
@@ -1,4 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+const DB = new PrismaClient();
+
+const selectAll = async () => {
+  try {
+    const notice = await DB.tbl_notice.findMany();
+    await DB.$disconnect();
+    return notice;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 const createNotice = async (data) => {
   console.log(data);
+  await DB.tbl_notice.create({ data: data });
 };
-export { createNotice };
+export { createNotice, selectAll };
--- nextjs-hello-001/src/app/notice/page.js
+++ nextjs-hello-001/src/app/notice/page.js
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import { selectAll } from "../api/notice";
 
 // import styles from "../css/NoticeInput.css";
-export default () => {
+export default async () => {
+  const noticeList = await selectAll();
+  const viewList = noticeList.map((item) => {
+    return (
+      <li>
+        <span>{item.m_author}</span>
+        <span>{item.m_subject}</span>
+      </li>
+    );
+  });
   return (
     <div>
+      <ul>{viewList}</ul>
       <h1>공지사항 리스트</h1>
       <Link href="/notice/input">공지사항 작성</Link>
     </div>
